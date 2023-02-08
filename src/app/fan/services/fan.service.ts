a5ddedf5bde4b6f6ecaec8367313c68c3007111a
Carole Gérard <gerard.carole@skynet.be>
2023-02-08 22:09:38 +0100

    service fan

--- src/app/fan/services/fan.service.ts
+++ src/app/fan/services/fan.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Fan } from '../types/Fan';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FanService {
 
+  private _fan: Fan[] = [
+    { name: 'Pierre', birthDate: new Date("1996-05-13"), series: ['The Boys', 'One piece', 'Trigun'] },
+    { name: 'Ryan', birthDate: new Date("1998-04-13"), series: ['The Boys', 'One piece', 'Naruto'] },
+  ];
+
   constructor() { }
+
+  getAll() {
+    return this._fan;
+  }
+
+  getOne(name: string) {
+    return this._fan.find((fan) => fan.name === name);
+  }
+  
 }
